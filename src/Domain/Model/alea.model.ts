4056f7bb9009bebd748bae11daa48c0a27b214e3
Kevin brun <kbrun.pro@gmail.com>
2024-10-01 18:11:30 +0200

    Alea entity

--- src/Domain/Model/alea.model.ts
+++ src/Domain/Model/alea.model.ts
@@ -1,1 +1,10 @@
-export class Alea {}
+import { Entity } from 'typeorm';
+
+@Entity()
+export class Alea {
+  id: number;
+  name: string;
+  legend: string;
+  disponible: boolean;
+  keywords: string[];
+}
